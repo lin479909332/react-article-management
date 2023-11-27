e59984daef9ba9fab2e9ed95032c14d375b59727
lin479909332 <lin479909332@gmail.com>
2023-11-27 21:55:57 +0800

    feat(Login/index.jsx): 搭建Form表单结构

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import { Card } from 'antd'
+import { Card, Form, Input, Checkbox, Button } from 'antd'
 import logo from '@/assets/logo.png'
 import './index.scss'
 const Login = () => {
@@ -7,6 +7,24 @@ const Login = () => {
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
+        <Form initialValues={{ remember: true }}>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
     </div>
   )
