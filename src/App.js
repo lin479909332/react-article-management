bec0f1f83d349086a375d08fdeae5be99ec95dd9
lin479909332 <lin479909332@gmail.com>
2023-11-27 21:31:50 +0800

    feat(App.js): 测试antd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Button } from 'antd'
 import './App.css'
 import Layout from './pages/Layout'
 import Login from './pages/Login'
@@ -7,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Button type="primary">Button</Button>
         <Routes>
           <Route path="/" element={<Layout />} />
           <Route path="/login" element={<Login />} />
