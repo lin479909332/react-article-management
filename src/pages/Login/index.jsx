b576b32c37cf2022406778e9d5bf6c4da5568753
lin479909332 <lin479909332@gmail.com>
2023-11-27 22:06:37 +0800

    feat(Login/index.jsx): 实现登录表单的校验

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,12 +7,33 @@ const Login = () => {
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form initialValues={{ remember: true }}>
-          <Form.Item>
+        <Form initialValues={{ remember: true }} validateTrigger={['onBlur', 'onChange']}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: '请输入手机号' },
+              {
+                pattern:
+                  /^1((34[0-8])|(8\d{2})|(([35][0-35-9]|4[579]|66|7[35678]|9[1389])\d{1}))\d{7}$/,
+                message: '手机号格式不正确',
+                validateTrigger: 'onBlur',
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
+          <Form.Item
+            name="password"
+            rules={[
+              { required: true, message: '请输入密码' },
+              {
+                len: 6,
+                message: '密码格式不正确',
+                validateTrigger: 'onBlur',
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入密码" />
           </Form.Item>
           <Form.Item name="remember" valuePropName="checked">
             <Checkbox className="login-checkbox-label">
