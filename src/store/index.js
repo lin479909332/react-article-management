1d5c88482bfe8a73b3e2ec12e21102183ff3bd70
lin479909332 <lin479909332@gmail.com>
2023-11-28 19:57:06 +0800

    feat(store/index.js): RootStore实例化

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginStore from './login.Store'
+class RootStore {
+  constructor() {
+    this.loginStore = new LoginStore()
+  }
+}
+
+// 实例化
+const rootStore = new RootStore()
+const context = React.createContext(rootStore)
+const useStore = React.useContext(context)
+
+export { useStore }
