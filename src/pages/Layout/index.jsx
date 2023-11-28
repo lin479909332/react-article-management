d7476fa2a65498eb57c56ab4087707b223d661f2
lin479909332 <lin479909332@gmail.com>
2023-11-28 21:34:53 +0800

    feat(Layout/index.jsx): 新增路由二级路由的跳转配置

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,6 +1,6 @@
 import { Layout, Menu, Popconfirm } from 'antd'
 import { HomeOutlined, DiffOutlined, EditOutlined, LogoutOutlined } from '@ant-design/icons'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 import './index.scss'
 const { Header, Sider } = Layout
 
@@ -27,13 +27,13 @@ const GeekLayout = () => {
             style={{ height: '100%', borderRight: 0 }}
           >
             <Menu.Item icon={<HomeOutlined />} key="1">
-              数据概览
+              <Link to="/">数据概览</Link>
             </Menu.Item>
             <Menu.Item icon={<DiffOutlined />} key="2">
-              内容管理
+              <Link to="/article">内容管理</Link>
             </Menu.Item>
             <Menu.Item icon={<EditOutlined />} key="3">
-              发布文章
+              <Link to="/publish">发布文章</Link>
             </Menu.Item>
           </Menu>
         </Sider>
