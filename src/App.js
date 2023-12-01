775b37a2a2ba6f8c84ec99b69927db6945d7ea4a
lin479909332 <lin479909332@gmail.com>
2023-12-02 01:59:38 +0800

    feat(App.js): 路由引入方式改为懒加载

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import GeekLayout from './pages/Layout'
-import Login from '@/pages/Login'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
 import { AuthComponent } from './components/AuthComponent'
 import { history, HistoryRouter } from '@/utils'
+import { lazy, Suspense } from 'react'
+
+const GeekLayout = lazy(() => import('@/pages/Layout'))
+const Login = lazy(() => import('@/pages/Login'))
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 
 function App() {
   return (
     <HistoryRouter history={history}>
       <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <AuthComponent>
-                <GeekLayout />
-              </AuthComponent>
-            }
-          >
-            <Route index element={<Home />} />
-            <Route path="article" element={<Article />} />
-            <Route path="publish" element={<Publish />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div style={{ textAlign: 'center', color: 'red', marginTop: 200 }}>Loading...</div>
+          }
+        >
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <AuthComponent>
+                  <GeekLayout />
+                </AuthComponent>
+              }
+            >
+              <Route index element={<Home />} />
+              <Route path="article" element={<Article />} />
+              <Route path="publish" element={<Publish />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Suspense>
       </div>
     </HistoryRouter>
   )
